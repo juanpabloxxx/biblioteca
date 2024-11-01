ccb7d6be7c20d0020e97e0573b18efd8a3991d50
“Juan”pabloxxx <122044165@upq.edu.mx>
2024-10-31 19:56:36 -0600

    aqui definimos las rutas d ela libreria

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,11 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\BibliotecaController;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [BibliotecaController::class, 'index'])->name('principal');
+Route::get('/registro', [BibliotecaController::class, 'create'])->name('libros.create');
+Route::post('/registro', [BibliotecaController::class, 'store'])->name('libros.store');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
